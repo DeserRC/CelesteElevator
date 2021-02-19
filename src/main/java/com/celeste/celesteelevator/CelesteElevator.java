a2bf1bca3425b8891020d2957f771eaef115710d
DeserRC <DeserRC@gmail.com>
2021-02-19 03:03:59 -0300

    Initial Commit

---
+++ src/main/java/com/celeste/celesteelevator/CelesteElevator.java
@@ -0,0 +1,21 @@
+package com.celeste.celesteelevator;
+
+import com.celeste.ServerPlugin;
+
+public class CelesteElevator extends ServerPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
